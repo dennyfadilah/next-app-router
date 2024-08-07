e0964d1cd8a8ee5e5028f65b6619e0753c72de68
dennyfadilah <dennyfadilah99@gmail.com>
2024-08-07 23:59:26 +0700

    Route Group

---
+++ src/app/product/[[...slug]]/page.tsx
@@ -0,0 +1,16 @@
+type DetailProductPageProps = { params: { slug: string } };
+
+export default function DetailProductPage({ params }: DetailProductPageProps) {
+	return (
+		<>
+			<h1>{params.slug ? "Detail Product Page" : "Product Page"}</h1>
+			{params.slug && (
+				<>
+					<h2>Category: {params.slug[0]}</h2>
+					<h2>Gender: {params.slug[1]}</h2>
+					<h2>Id: {params.slug[2]}</h2>
+				</>
+			)}
+		</>
+	);
+}
---
+++ src/app/template.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { useState } from "react";
+
+export default function Template({ children }: { children: React.ReactNode }) {
+	const [state, setState] = useState(0);
+
+	return (
+		<>
+			{/* <h1>Template {state}</h1>
+            <button type="button" onClick={()=> setState(state+1)}>Klik</button> */}
+			<div>{children}</div>
+		</>
+	);
+}
